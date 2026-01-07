6f8c49aaf92efaa7b9f0aedbaadb5bab6f814ba5
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-07 14:23:59 +0530

    update

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -42,7 +42,7 @@ const addUser = async (req, res) => {
 const deleteUser = async (req, res) => {
   try {
     const deletedUser = await userModel.findByIdAndDelete(req.params.id);
-    console.log(deletedUser)
+    console.log(deletedUser);
     if (deletedUser) {
       res.status(200).json({
         message: "user deleted..",
@@ -58,10 +58,53 @@ const deleteUser = async (req, res) => {
     });
   }
 };
+const updateUser = async (req, res) => {
+  //db.users.update(id,data)
+  //id == req.params.id
+  //data=  req.body
+  //const updatedUser = await userModel.findByIdAndUpdate(req.params.id, req.body);
+  const updatedUser = await userModel.findByIdAndUpdate(req.params.id, req.body,{new:true});
+  if (updatedUser) {
+    res.json({
+      message: "user updated successfully...",
+      data: updatedUser,
+    });
+  } else {
+    res.json({
+      message: "user is not updated...",
+    });
+  }
+};
+
+//if hobby is already avaialble it should not add....
+const addNewHobby = async(req,res)=>{
+
+    const hobby = req.body.hobby
+    const id = req.params.id
+
+    try{
+    const updatedUserafteraddhobby =await userModel.findByIdAndUpdate(id,{$push:{hobbies:hobby}},{new:true})
+    //if else
+      res.json({
+        message:"hobby addedd successfull",
+        data:updatedUserafteraddhobby
+      })
+    }catch(err){
+      res.json({
+        message:"errir while adding hobby ",
+        err:err
+      })
+    }
+
+
+}
+
 
 module.exports = {
   getAllUsers,
   getUserById,
   addUser,
   deleteUser,
+  updateUser,
+  addNewHobby
 };
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,4 +4,6 @@ router.get("/users",userController.getAllUsers)
 router.get("/user/:id",userController.getUserById)
 router.post("/user",userController.addUser)
 router.delete("/user/:id",userController.deleteUser)
+router.put("/user/:id",userController.updateUser)
+router.put("/user/addhobby/:id",userController.addNewHobby)
 module.exports = router
